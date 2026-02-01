19fcf3ab3224eb5051d9b955fdefe5ac6d657acb
Winnie Mutunga <soccer.knocker@gmail.com>
2026-02-01 16:41:10 -0500

    removing 1min test from breakshit. eventual merge with main

--- lib/constants.ts
+++ lib/constants.ts
@@ -375,5 +375,5 @@ export const MEDICINE_CATEGORIES: { value: MedicineCategory; label: string; icon
 /**
  * Interval for showing the sync reminder modal (48 hours)
  */
-// TODO: revert to 48 * 60 * 60 * 1000 after testing
-export const SYNC_REMINDER_INTERVAL_MS = 1 * 60 * 1000;
+// 1min TEST: 1 * 60 * 1000
+export const SYNC_REMINDER_INTERVAL_MS = 48 * 60 * 60 * 1000;
