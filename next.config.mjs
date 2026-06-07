3bff10c77182f4ca4db1957da914d8e3e228c50c
thecrimsonelephant <soccer.knocker@gmail.com>
2026-06-07 01:29:30 +0000

    Deploying to main from @ theselfstudy/cadence@9db0ad6c0912ed8f48b77db33135aa1ad156bf32 🚀

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/cadence',
-  trailingSlash: true,
-  images: { unoptimized: true },
-};
-
-export default nextConfig;
